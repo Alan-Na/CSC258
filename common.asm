251546fc3f3f5bb5885418e17e740f301c1adf93
Alan-Na <xinengn886@gmail.com>
2025-03-24 19:11:31 -0400

    common file created

---
+++ common.asm
@@ -0,0 +1,11 @@
+
+##############################################################################
+    .data
+ADDR_DSPL:      .word  0x10008000  # Bitmap address
+ADDR_KBRD:      .word  0xffff0000  # Ketboard address
+COLOR_GREY:     .word  0x808080    # Grey
+COLOR_RED:      .word  0xFF0000    # Red
+COLOR_GREEN:    .word  0x00FF00    # Green
+COLOR_BLUE:     .word  0x0000FF    # Blue
+COLORS:         .word  0xFF0000, 0x00FF00, 0x0000FF  # Colours array
+
