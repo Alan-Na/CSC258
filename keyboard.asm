e6bfbfa646768c16400e4359f4eae17b3ae54acc
Yi Chen Liu <ychen.liu@mail.utoronto.ca>
2025-03-25 22:18:37 -0400

    left right collision finish

--- keyboard.asm
+++ keyboard.asm
@@ -30,14 +30,38 @@ keyboard_input:                     # A key is pressed
     b testmain2
 
 respond_to_A:
+  addi $t1, $s0, -1
+  beq $t1, 2, End_A
   addi $s0, $s0, -1      # Move the X_position 1 left
   jr $ra
+
+End_A:
+  jr $ra
+  
 respond_to_D:
+  addi $t1, $s0, 1
+  beq $s4, 0, vertical_D
+  beq $s4, 1, horizontal_D
   addi $s0, $s0, 1       # Move the X_position 1 right
   jr $ra
+
+horizontal_D:
+  beq $t1, 19, End_D
+  addi $s0, $s0, 1       # Move the X_position 1 right
+  jr $ra
+
+vertical_D:
+  beq $t1, 20, End_D
+  addi $s0, $s0, 1       # Move the X_position 1 right
+  jr $ra  
+  
+End_D:
+  jr $ra
+  
 respond_to_S:
   addi $s1, $s1, 1       # Move the Y_position 1 down
   jr $ra
+  
 respond_to_W:
   beq $s4, 0, vertical_capsule     # Check if the capsule is vertical
   beq $s4, 1, horizontal_capsule   # Check if the capsule is horizontal
@@ -50,10 +74,13 @@ vertical_capsule:
   add $s3, $t8, $zero
   li $s4, 1                # Change the horizontal status to 1
   jr $ra
+
 horizontal_capsule:
   addi $s1, $s1, -1        # Move y position up by 1
   li $s4, 0                # Change the horizontal status to 0
   jr $ra
+
+  
 respond_to_Q:
 	li $v0, 10                      # Quit gracefully
 	syscall
